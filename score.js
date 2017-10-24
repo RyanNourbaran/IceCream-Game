47560aeb4176ca7571cedca9a5ea8ecabd11914d
Ryan Nourbaran <Ryan.nourbaran@hotmail.com>
2017-10-24 06:52:23 -0400

    no more outline

--- score.js
+++ score.js
@@ -5,13 +5,13 @@ function Score(cone) {
 
 
     this.show = function () {
-        textSize(35);
+        textSize(50);
         stroke('brown');
-        strokeWeight(2);
+        strokeWeight(0);
         textStyle(BOLD);
         fill(255,242,55);
         text(this.points,750,0,100,50);
-        textSize(35);
+        textSize(50);
         text('High-Score: ' +this.maxscore,0,1080);
     }
 
